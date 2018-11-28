f1eda46a8216f5f16dab343706c534b15d098473
Nathan Neuzil <n64134@kuha.kumed.com>
2018-11-28 15:26:28 -0600

    Updated RankedBook Class

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,20 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
-   }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (base.Students.Count < 5)
+                throw new InvalidOperationException();
+            if (averageGrade >= .8)
+                return 'A';
+            else if (averageGrade >= .6 && averageGrade < .8)
+                return 'B';
+            else if (averageGrade >= .4 && averageGrade < .6)
+                return 'C';
+            else if (averageGrade >= .2 && averageGrade < .4)
+                return 'D';
+            return 'F';
+        }
+    }
 }
